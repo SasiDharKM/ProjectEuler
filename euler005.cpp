5c93fc3c4b6374976c195c3ac29eff1c11cab6ce
SasiDharKM <sasidhar.km97@gmail.com>
2017-05-15 04:25:50 +0530

    Added a solution

---
+++ euler005.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+long long lcm(int n){
+    long long ans=1;
+    for(long long i=1;i<=n;i++)
+        ans= (ans*i)/(__gcd(ans,i));
+    return ans;
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout<<lcm(n)<<endl;
+        }
+    
+    
+    return 0;
+}
